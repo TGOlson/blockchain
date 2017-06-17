39a2d477670cd9465b07a6f3daf5c8a38a1eed26
Tyler Olson <tydotg@gmail.com>
2017-06-17 00:00:08 -0700

    mine test chain

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,52 +1,92 @@
 module Main (main) where
 
-import qualified Data.Aeson                               as Aeson
-import qualified Data.ByteString.Lazy                     as Lazy
-import qualified Data.List.NonEmpty                       as NonEmpty
+import           Control.Monad                     (void)
+import qualified Data.Aeson                        as Aeson
+import qualified Data.ByteString.Lazy              as Lazy
+-- import qualified Data.List.NonEmpty                as NonEmpty
+import           Data.Monoid                       ((<>))
+import qualified Data.Time.Clock                   as Time
 
-import qualified Data.Blockchain.Core.Blockchain          as Blockchain
-import qualified Data.Blockchain.Core.Builder.Transaction as Tx
-import qualified Data.Blockchain.Core.Crypto              as Crypto
-import qualified Data.Blockchain.Core.Types               as Blockchain
-import qualified Data.Blockchain.Mining.Block             as Mining
+import qualified Data.Blockchain.Core.Blockchain   as Blockchain
+-- import qualified Data.Blockchain.Core.Builder.Transaction as Tx
+import qualified Data.Blockchain.Core.Crypto       as Crypto
+import qualified Data.Blockchain.Core.Types        as Blockchain
+import qualified Data.Blockchain.Mining.Block      as Mining
+import qualified Data.Blockchain.Mining.Blockchain as Mining
 
 main :: IO ()
-main = generateSingletonChain
+main = do
+    (Crypto.KeyPair pub priv) <- Crypto.generate
+    writeJSON "data/test_blockchain_priv_key.json" priv
 
-generateSingletonChain :: IO ()
-generateSingletonChain = do
-    singleton <- singletonBlockchain
+    putStrLn "Creating blockchain with block 0"
+    blockchain <- Mining.createBlockchain config
 
-    block1 <- readJSON "data/block_1a.json"
-    let blockchain' = throwLeft $ Blockchain.addBlock block1 singleton
+    void $ runMining pub blockchain
+  where
+    config = Blockchain.defaultConfig
+        { Blockchain.initialMiningReward           = 100000
+        , Blockchain.difficultyRecalculationHeight = 10
+        , Blockchain.miningRewardHalvingHeight     = 20
+        }
 
-    pk <- block1ACoinbasePrivateKey
-    let pubKey = coinbasePublicKey block1
-        keyPair = Crypto.KeyPair pubKey pk
+runMining :: Crypto.PublicKey -> Blockchain.Blockchain Blockchain.Validated -> IO (Blockchain.Blockchain Blockchain.Validated)
+runMining pubKey chain = do
+    let chainLength = length $ Blockchain.longestChain chain
 
-    (Crypto.KeyPair targetPublicKey privKey) <- Crypto.generate
-    tx <- throwLeft <$> Tx.createSimpleTransaction keyPair targetPublicKey 90 0 blockchain'
+    putStrLn $ "Mining block #" <> show chainLength
 
-    block2 <- throwLeft <$> Mining.mineBlock pubKey (pure tx) blockchain'
+    start <- Time.getCurrentTime
+    block <- throwLeft <$> Mining.mineEmptyBlock pubKey chain
+    end   <- Time.getCurrentTime
 
-    writeJSON "data/block_2a.json" block2
-    writeJSON "data/block_2a_coinbase_private_key.json" privKey
+    let h = Crypto.hash (Blockchain.blockHeader block)
 
-coinbasePublicKey :: Blockchain.Block -> Crypto.PublicKey
-coinbasePublicKey = Blockchain.signaturePubKey . NonEmpty.head . Blockchain.coinbaseTransactionOut . Blockchain.coinbaseTransaction
+    putStrLn "Found next block"
+    putStrLn $ "  Hash: " <> show h
+    putStrLn $ "  Elapsed time: " <> show (Time.diffUTCTime end start)
 
-block1ACoinbasePrivateKey :: IO Crypto.PrivateKey
-block1ACoinbasePrivateKey = readJSON "data/block_1a_coinbase_private_key.json"
+    case Blockchain.addBlock block chain of
+        Right chain' -> writeJSON "data/test_blockchain.json" chain' >> runMining pubKey chain'
+        Left e       -> error $ "Unexpected error while mining: " <> show e
 
 
-singletonBlockchain :: IO (Blockchain.Blockchain Blockchain.Validated)
-singletonBlockchain = throwLeft . Blockchain.validate <$> readJSON "data/singleton_blockchain.json"
+
+
+-- generateSingletonChain :: IO ()
+-- generateSingletonChain = do
+--     singleton <- singletonBlockchain
+--
+--     block1 <- readJSON "data/block_1a.json"
+--     let blockchain' = throwLeft $ Blockchain.addBlock block1 singleton
+--
+--     pk <- block1ACoinbasePrivateKey
+--     let pubKey = coinbasePublicKey block1
+--         keyPair = Crypto.KeyPair pubKey pk
+--
+--     (Crypto.KeyPair targetPublicKey privKey) <- Crypto.generate
+--     tx <- throwLeft <$> Tx.createSimpleTransaction keyPair targetPublicKey 90 0 blockchain'
+--
+--     block2 <- throwLeft <$> Mining.mineBlock pubKey (pure tx) blockchain'
+--
+--     writeJSON "data/block_2a.json" block2
+--     writeJSON "data/block_2a_coinbase_private_key.json" privKey
+
+-- coinbasePublicKey :: Blockchain.Block -> Crypto.PublicKey
+-- coinbasePublicKey = Blockchain.signaturePubKey . NonEmpty.head . Blockchain.coinbaseTransactionOut . Blockchain.coinbaseTransaction
+--
+-- block1ACoinbasePrivateKey :: IO Crypto.PrivateKey
+-- block1ACoinbasePrivateKey = readJSON "data/block_1a_coinbase_private_key.json"
+--
+--
+-- singletonBlockchain :: IO (Blockchain.Blockchain Blockchain.Validated)
+-- singletonBlockchain = throwLeft . Blockchain.validate <$> readJSON "data/singleton_blockchain.json"
 
 writeJSON :: Aeson.ToJSON a => FilePath -> a -> IO ()
 writeJSON path = Lazy.writeFile path . Aeson.encode
 
-readJSON :: Aeson.FromJSON a => FilePath -> IO a
-readJSON path =  throwLeft . Aeson.eitherDecode <$> Lazy.readFile path
+-- readJSON :: Aeson.FromJSON a => FilePath -> IO a
+-- readJSON path =  throwLeft . Aeson.eitherDecode <$> Lazy.readFile path
 
 throwLeft :: Show a => Either a b -> b
 throwLeft = either (error . show) id
