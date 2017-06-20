ba64e88b54e308207ee7aaf3051fdcd8b0752395
Tyler Olson <tolson@box.com>
2017-06-19 21:09:58 -0700

    test stats

--- test/Integration/Stats.hs
+++ test/Integration/Stats.hs
@@ -11,14 +11,18 @@ import           Data.Monoid                     ((<>))
 import           Data.Ratio                      (Ratio)
 import qualified Data.Time.Clock                 as Time
 import           Data.Word                       (Word)
+import           Numeric.Natural                 (Natural)
 
 import qualified Data.Blockchain.Core.Blockchain as B
 import qualified Data.Blockchain.Core.Types      as B
 
 data BlockchainStats = BlockchainStats
-    { numBlocks   :: Word
-    , totalTime   :: Time.NominalDiffTime
-    , totalHashes :: Word
+    { numBlocks             :: Word
+    , totalTime             :: Time.NominalDiffTime
+    , totalHashes           :: Word
+    , difficultyProgression :: [Natural]
+    , rewardProgression     :: [Word]
+    , config                :: B.BlockchainConfig
     }
 
 printStats :: B.Blockchain B.Validated -> IO ()
@@ -28,13 +32,22 @@ printStats blockchain = do
         outputsP = List.intercalate "\n" $ (\(k, v) -> show k <> ": " <> show v) <$> outputs
 
     putStrLn $ List.intercalate "\n"
-        [ "Final test chain mining stats"
-        , "  Length:             " <> show (numBlocks stats)
-        , "  Total time:         " <> show (totalTime stats)
-        , "  Time per block:     " <> show (timePerBlock stats)
-        , "  Total hashes:       " <> show (totalHashes stats)
-        , "  Hashes per block:   " <> show (hashesPerBlock stats)
-        , "  Hash rate (hash/s): " <> show (hashRate stats)
+        [ mempty
+        , "Final test chain mining stats"
+        , "  Length:                 " <> show (numBlocks stats)
+        , "  Total time:             " <> show (totalTime stats)
+        , "  Time per block:         " <> show (timePerBlock stats)
+        , "  Total hashes:           " <> show (totalHashes stats)
+        , "  Hashes per block:       " <> show (hashesPerBlock stats)
+        , "  Hash rate (hash/s):     " <> show (hashRate stats)
+        , "  Last difficulty:        " <> show (last (difficultyProgression stats))
+        , "  Difficulty progression: " <> show (difficultyProgression stats)
+        , "  Last reward:            " <> show (last (rewardProgression stats))
+        , "  Reward progression:     " <> show (rewardProgression stats)
+        , mempty
+        , "Config used"
+        , show (config stats)
+        , mempty
         , "Current address values"
         , outputsP
         ]
@@ -43,13 +56,17 @@ printStats blockchain = do
 computeStats :: B.Blockchain B.Validated -> BlockchainStats
 computeStats blockchain = BlockchainStats{..}
   where
-    numBlocks   = fromIntegral (length mainChain)
-    totalTime   = Time.diffUTCTime (B.time lastHeader) (B.time firstHeader)
-    totalHashes = fromIntegral $ sum $ (succ . B.nonce) <$> headers -- Add 1 to each nonce to account for first hash
-    mainChain   = NonEmpty.toList $ B.longestChain blockchain
-    headers     = B.blockHeader <$> mainChain
-    firstHeader = head headers
-    lastHeader  = last headers
+    numBlocks             = fromIntegral (length mainChain)
+    totalTime             = Time.diffUTCTime (B.time lastHeader) (B.time firstHeader)
+    totalHashes           = fromIntegral $ sum $ (succ . B.nonce) <$> headers -- Add 1 to each nonce to account for first hash
+    mainChain             = NonEmpty.toList $ B.longestChain blockchain
+    headers               = B.blockHeader <$> mainChain
+    coinbaseTxOuts        = B.coinbaseTransactionOut . B.coinbaseTransaction <$> mainChain
+    firstHeader           = head headers
+    lastHeader            = last headers
+    difficultyProgression = List.nub $ (B.unDifficulty . B.difficulty) <$> headers
+    rewardProgression     = List.nub $ (sum . fmap B.value) <$> coinbaseTxOuts
+    config                = B.blockchainConfig blockchain
 
 timePerBlock :: BlockchainStats -> Time.NominalDiffTime
 timePerBlock BlockchainStats{..} = totalTime / fromIntegral numBlocks
