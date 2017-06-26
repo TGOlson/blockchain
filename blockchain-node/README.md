ce744cc01e5baaa79201d36d5ab4a3cb8d636cf6
Tyler Olson <tolson@box.com>
2017-06-26 09:55:29 -0700

    add endpoint for posting new blocks

--- blockchain-node/README.md
+++ blockchain-node/README.md
@@ -1,3 +1,8 @@
 # blockchain-node
 
 Network client that creates blocks, verifies entities and persists the blockchain.
+
+## todo
+
+* create `blockchain-api` for core api types
+  * can share between node/wallet
--- blockchain-node/lib/Network/Blockchain/Node.hs
+++ blockchain-node/lib/Network/Blockchain/Node.hs
@@ -2,18 +2,21 @@ module Network.Blockchain.Node
     ( main
     ) where
 
-import           Control.Concurrent.STM.TVar (TVar)
-import qualified Control.Concurrent.STM.TVar as TVar
+import           Control.Concurrent.STM      (atomically)
+import           Control.Concurrent.STM.TVar
 import           Control.Monad.Trans
-import qualified Data.Aeson                  as Aeson
+import           Data.Aeson
 import           Data.Blockchain
 import qualified Data.ByteString.Lazy        as Lazy
 import           Data.Monoid                 ((<>))
 import           Data.Proxy                  (Proxy (..))
 import qualified Network.Wai.Handler.Warp    as Warp
 import           Servant
+import           System.Environment          (getArgs)
 
-type NodeAPI = "blockchain" :> Get '[JSON] (Blockchain Validated)
+type NodeAPI
+    =     "blockchain" :> Get '[JSON] (Blockchain Validated)
+    :<|>  "block" :> ReqBody '[JSON] Block :> Post '[JSON] ()
 
 data Config = Config
     { blockchainPath :: FilePath
@@ -25,35 +28,48 @@ nodeAPI :: Proxy NodeAPI
 nodeAPI = Proxy
 
 main :: IO ()
-main = do
-    let runLog = putStrLn
-    let blockchainPath = "data/singleton_blockchain.json"
+main = getArgs >>= \case -- TODO: opt-parse or something
+    [blockchainPath] -> do
+        let runLog = putStrLn
 
-    runLog $ "Running node using blockchain at path: " <> blockchainPath
+        runLog $ "Running node using blockchain at path: " <> blockchainPath
 
-    bs <- Lazy.readFile blockchainPath
+        bs <- Lazy.readFile blockchainPath
 
-    blockchainUnvalidated <- either
-        (\e -> error $ "Error: unable to decode blockchain: " <> show e)
-        return (Aeson.eitherDecode bs)
+        blockchainUnvalidated <- either
+            (\e -> error $ "Error: unable to decode blockchain: " <> show e)
+            return (eitherDecode bs)
 
-    blockchain <- either
-        (\e -> error $ "Error: invalid blockchain: " <> show e)
-        return (validate blockchainUnvalidated)
+        blockchain <- either
+            (\e -> error $ "Error: invalid blockchain: " <> show e)
+            return (validate blockchainUnvalidated)
 
-    blockchainVar <- TVar.newTVarIO blockchain
+        blockchainVar <- newTVarIO blockchain
 
-    runServer Config{..}
+        runServer Config{..}
+    _ -> error "invalid args"
   where
     runServer = Warp.run 8000 . serve nodeAPI . server
 
 -- TODO: ReaderT
+-- TODO: request logging
 server :: Config -> Server NodeAPI
-server = getBlockchain
+server config = getBlockchain config
+           :<|> insertBlock config
 
 getBlockchain :: Config -> Handler (Blockchain Validated)
-getBlockchain = liftIO . TVar.readTVarIO . blockchainVar
+getBlockchain = liftIO . readTVarIO . blockchainVar
 
+insertBlock :: Config -> Block -> Handler ()
+insertBlock config block = do
+    blockchain <- getBlockchain config
 
-throwLeft :: Show a => Either a b -> b
-throwLeft = either (error . show) id
+    case addBlock block blockchain of
+        Right blockchain' -> liftIO $ atomically $ writeTVar (blockchainVar config) blockchain' -- TODO: persist, use sqlite
+        Left e            -> throwError (addBlockError e)
+
+addBlockError :: BlockException -> ServantErr
+addBlockError e = err400 { errBody = body }
+  where
+    body = encode $ object ["error" .= msg ]
+    msg  = "Invalid block: " <> show e
